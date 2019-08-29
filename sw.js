5f0942a29030290ea69e885fd6f6eb1b50fd3b94
Marco Antonio Trujillo <matrujillov@gmail.com>
2019-08-29 00:08:32 -0400

    add sw.js

---
+++ sw.js
@@ -0,0 +1,65 @@
+
+
+const STATIC_CACHE    = 'static-v1';
+const DYNAMIC_CACHE   = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+
+const APP_SHELL = [
+    // '/',
+    'index.html',
+    'src/css/style.css',
+    'src/img/logo.png',
+    'src/img/sprite-icos.png',
+    'src/js/app.js',
+];
+
+const APP_SHELL_INMUTABLE = [
+    'https://fonts.googleapis.com/css?family=Yantramanav:300,500',
+    'https://kit.fontawesome.com/1a30776616.js',
+    'src/css/animate.css',
+    'src/js/jquery-3.4.1.min.js'
+];
+
+
+
+self.addEventListener('install', e => {
+
+
+    const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL ));
+
+    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL_INMUTABLE ));
+
+
+
+    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
+
+});
+
+
+self.addEventListener('activate', e => {
+
+    const respuesta = caches.keys().then( keys => {
+
+        keys.forEach( key => {
+
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+
+        });
+
+    });
+
+    e.waitUntil( respuesta );
+
+});
+
+
+
