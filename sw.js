a5fa13841b96927b8ce7d339b7baf36bbcb3b829
Marco Antonio Trujillo <matrujillov@gmail.com>
2019-08-30 11:41:42 -0400

    update static cache

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('src/js/sw-utils.js');
 
-const STATIC_CACHE    = 'static-v2';
+const STATIC_CACHE    = 'static-v3';
 const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
